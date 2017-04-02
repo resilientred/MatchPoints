945996481edd599fd8eb68fd0d965f9973e13618
makenneth <kenneth.km.ma@gmail.com>
2017-04-01 18:10:54 -0700

    Added time limit for temp storage

--- src/redux/modules/tempSession.js
+++ src/redux/modules/tempSession.js
@@ -3,7 +3,10 @@ export const RESTORE_TEMP_SESSION = 'mp/tempSession/RESTORE_TEMP_SESSION';
 export const saveTempSession = () => {
   return (dispatch, getState) => {
     const { newSession } = getState();
-    window.localStorage.setItem('matchpoint_session', JSON.stringify(newSession));
+    window.localStorage.setItem(
+      'matchpoint_session',
+      JSON.stringify(Object.assign({}, newSession, { date: new Date() })
+    );
   };
 };
 
@@ -12,7 +15,15 @@ export const clearTempSession = () => {
 };
 
 export const getTempSession = () => {
-  const session = window.localStorage.getItem('matchpoint_session');
+  let session = window.localStorage.getItem('matchpoint_session');
+  if (session) {
+    const date = JSON.parse(session).date;
+
+    if (!date || date - new Date() > 60 * 1000 * 15) {
+      clearTempSession();
+      session = null;
+    }
+  }
   return session;
 };
 
